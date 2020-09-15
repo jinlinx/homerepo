e364e3fd5cea39b4f783af5e67c0c0522df3a6d3
Gang Zhang <gzhangx@hotmail.com>
2020-09-15 15:49:33 -0400

    missing file

---
+++ test.js
@@ -0,0 +1,11 @@
+const qa=require('./lib/getSheet');
+
+
+//const res=qa.authorize1GetUrl(credentials.googleSheet.installed);
+//qa.authorize2CodeToToken(credentials.googleSheet.installed,'4/4AHFokhwhKXHAifcHMA2A6JtZ6aRF_I9G9aOnKuGuK9f3cyF-ZESCy8')
+const sheet=qa.createSheet();
+
+sheet
+
+
+
