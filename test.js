96ef9508830e654d93f510105e4e457b1fe69831
Gang Zhang <gzhangx@hotmail.com>
2020-09-15 16:52:02 -0400

    test

--- test.js
+++ test.js
@@ -5,7 +5,11 @@ const qa=require('./lib/getSheet');
 //qa.authorize2CodeToToken(credentials.googleSheet.installed,'4/4AHFokhwhKXHAifcHMA2A6JtZ6aRF_I9G9aOnKuGuK9f3cyF-ZESCy8')
 const sheet=qa.createSheet();
 
-sheet
+
+sheet.readRanges('1lXOIPkIhpMRLtQTg7CXkhE2M3__vJmGkwlHKTNk1rO4',[`'TenantInfo'!A1:E2500`]).then(res => {
+    console.log('done2');
+    console.log(res.data.valueRanges[0].values);
+});
 
 
 
