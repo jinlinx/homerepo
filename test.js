39f592e9613f596687e394f7cc19c46b3d155324
Gang Zhang <gzhangx@hotmail.com>
2020-09-21 22:07:59 -0400

    test

--- test.js
+++ test.js
@@ -1,3 +1,40 @@
+//create database PM;
+//CREATE USER 'jjuser'@'localhost' IDENTIFIED BY '12345';
+//GRANT ALL PRIVILEGES ON PM.*TO 'jjuser'@'%' IDENTIFIED BY '12345';
+//create table tenant(email varchar(50), address varchar(50), phone varchar(50));
+const mysql=require('mysql');
+const conn = mysql.createConnection({
+  host:'192.168.1.115',
+  user:'jjuser',
+  password: '12345',
+  database: "PM"
+});
+
+function doQuery(sql) {
+  return new Promise((resolve, reject)=>{
+    conn.connect(function(err) {
+      if (err) {
+        console.log(err);
+        return err;
+      }
+      console.log("Connected!");
+      conn.query(sql,(err,result) =>{
+        if(err) return reject(err);
+        resolve(result);
+      });
+    });
+  });
+}
+
+async function test() {
+  const ten= await doQuery('select * from tenant');
+  console.log(ten);
+  conn.end();
+}
+return test();
+return;
+
+
 const qa=require('./lib/getSheet');
 
 
